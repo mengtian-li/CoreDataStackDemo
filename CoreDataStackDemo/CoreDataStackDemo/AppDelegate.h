556191bca46d02938aff33e0f941b3df22c94da1
mengtian-li <mengtian.li@hotmail.com>
2017-08-20 11:57:29 +0800

    [add] add projiect files

---
+++ CoreDataStackDemo/CoreDataStackDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataStackDemo
+//
+//  Created by LiMengtian on 20/08/2017.
+//  Copyright © 2017 LiMengtian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataStackDemo/CoreDataStackDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataStackDemo
+//
+//  Created by LiMengtian on 20/08/2017.
+//  Copyright © 2017 LiMengtian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
